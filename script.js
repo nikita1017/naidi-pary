5282b5ffcf50086e3af3a847318ea69e1aac7e25
nikita1017 <78160117+nikita1017@users.noreply.github.com>
2022-11-17 21:13:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let game = document.getElementById("game");
+let imgs = "";
+let heals = document.getElementById("heals");
+let serdze = 1;
+let images = [];
+for (let image = 0; image <= 11; image++) {
+  images.push("img/0 " + "(" + image + ")" + ".jpg");
+  images.push("img/0 " + "(" + image + ")" + ".jpg");
+}
+console.log(images);
+for (let i = 0; i <= 23; i++) {
+  // первый способ вывода нескольких фотографий
+//   imgs = imgs + '<img src="img/0 (' + i + ').jpg" alt="">';
+  // второй способ вывода нескольких фотографий
+  let img = document.createElement("img");
+  img.src = "img/vopros.jpg";
+  game.appendChild(img);
+  img.onclick=function(){
+    console.log("+1");
+    serdze++;
+    heals.innerHTML=("Попытки: " + serdze);
+    let image = 0;
+    img.src =images[i];
+  }
+}
